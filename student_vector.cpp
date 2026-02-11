27c558dba26d59d10db23ac4b744cb9986f05603
Adomas Šapoka <adomas.sapoka@gmail.com>
2026-02-11 22:41:17 +0200

    3 menu funkcija

--- student_vector.cpp
+++ student_vector.cpp
@@ -9,6 +9,15 @@
 
 using namespace std;
 
+vector<string> vardai = {"Jonas", "Adomas", "Vytautas", "Juozas", "Matas", "Mantas", "Dominykas", "Algirdas", "Gediminas", "Mindaugas", 
+    "Laura", "Eglė", "Edita", "Gabija", "Justina", "Aistė", "Rasa", "Gintarė", "Rūta", "Lina"};
+vector<string> pavardes_vyr = {"Pavardenis1", "Pavardenis2", "Pavardenis3", "Pavardenis4", "Pavardenis5", 
+    "Pavardenis6", "Pavardenis7", "Pavardenis8", "Pavardenis9", "Pavardenis10"
+};
+vector<string> pavardes_mot = {"Pavardenaitė1", "Pavardenaitė2", "Pavardenaitė3", "Pavardenaitė4", "Pavardenaitė5",
+    "Pavardenaitė6", "Pavardenaitė7", "Pavardenaitė8", "Pavardenaitė9", "Pavardenaitė10"
+};
+
 struct Student {
     string name;
     string surname;
@@ -93,6 +102,26 @@ Student enterStudent2() {
     return s;
 }
 
+Student enterStudent3(int hm_count) {
+    
+    Student s;
+    s.name = vardai[rand() % vardai.size()];
+    if (s.name.back() == 's') {
+        s.surname = pavardes_vyr[rand() % pavardes_vyr.size()];
+    } else {
+        s.surname = pavardes_mot[rand() % pavardes_mot.size()];
+    }
+
+    for (int j = 0; j < hm_count; j++) {
+        s.homework.push_back(rand() % 11);
+    }
+
+    s.exam = rand() % 11;
+
+    return s;
+    
+}
+
 double calculateMean(const vector<int> &hm, int exam) {
     if (hm.empty()) return exam*0.6;
     double s = 0;
@@ -119,9 +148,9 @@ double calculateMedian(vector<int> hm, int exam) {
 
 void printStudent(Student stu, int comm) {
     if (comm == 1) {
-        cout << left << setw(15) << stu.surname << setw(15) << stu.name << setw(10) << fixed << setprecision(2) << stu.mean << endl;
+        cout << left << setw(25) << stu.surname << setw(20) << stu.name << setw(15) << fixed << setprecision(2) << stu.mean << endl;
     } else if (comm == 2) {
-        cout << left << setw(15) << stu.surname << setw(15) << stu.name << setw(10) << fixed << setprecision(2) << stu.median << endl;
+        cout << left << setw(25) << stu.surname << setw(120) << stu.name << setw(15) << fixed << setprecision(2) << stu.median << endl;
     }
 
 }
@@ -179,7 +208,43 @@ int main() {
                     }
                 }
             }
-        } else if (menu_choice == 4) {
+        } else if (menu_choice == 3) {
+            int student_count;
+            cout << "Atsitiktinai sugeneruotų studentų skaičius: ";
+            while (true) {
+                if (!(cin >> student_count)) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cout << "Studentų skaičius turi būti sveikasis skaičius ne mažesnis už 1: ";
+                } else if (student_count < 1) {
+                    cout << "Studentų skaičius turi būti sveikasis skaičius ne mažesnis už 1: ";
+                } else {
+                    break;
+                }
+            }
+
+            int hm_count;
+            cout << "Atsitiktinai sugeneruotų namų darbų įvertinimų skaičius: ";
+            while (true) {
+                if (!(cin >> hm_count)) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cout << "Įvertinimų skaičius turi būti sveikasis skaičius ne mažesnis už 0: ";
+                } else if (hm_count < 0) {
+                    cout << "Įvertinimų skaičius turi būti sveikasis skaičius ne mažesnis už 0: ";
+                } else {
+                    break;
+                }
+            }
+
+            for (int i = 0; i < student_count; i++) {
+                Student s = enterStudent3(hm_count);
+                s.mean = calculateMean(s.homework, s.exam);
+                s.median = calculateMedian(s.homework, s.exam);
+                students.push_back(s);
+            }
+
+        } else {
             break;
         }
 
@@ -199,12 +264,12 @@ int main() {
         
         }
 
-        cout << left << setw(15) << "Pavardė" << setw(15) << "Vardas" << setw(10) << "Galutinis" << endl;
+        cout << left << setw(25) << "Pavardė" << setw(20) << "Vardas" << setw(15) << "Galutinis" << endl;
         cout << "-------------------------------------------------------------" << endl;
         for (const auto &stu : students) {
                 printStudent(stu, isvedimas);
         }
-
+        cout << "-------------------------------------------------------------" << endl;
         students.clear();
 
     }

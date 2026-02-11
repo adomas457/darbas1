4e067aa9c9eee2efd81eeb5becc1f5cc564eb624
Adomas Šapoka <adomas.sapoka@gmail.com>
2026-02-11 21:27:02 +0200

    menu prototipas

--- student_vector.cpp
+++ student_vector.cpp
@@ -96,40 +96,69 @@ void printStudent(Student stu, int comm) {
 
 int main() {
     vector<Student> students;
-    string comm = "y";
-    while (comm == "y") {
-        Student s = enterStudent();
-        s.mean = calculateMean(s.homework, s.exam);
-        s.median = calculateMedian(s.homework, s.exam);
-        students.push_back(s);
-        while (true) {
-            cout << "Įrašyti dar vieną studentą? (y/n): ";
-            cin >> comm;
-            if (comm == "y" || comm == "n") {
-                break;
-            }
-        }
-    }
-
-    int comm2;
+    int menu_choice;
     while (true) {
-        cout << "Išvesti rezultatus vidurkio ar medianos pavidalu? (1 - vid., 2 - med.): ";
-        if (cin>>comm2) {
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            if (comm2 == 1 || comm2 == 2 ) {
-                break;
-            }
-        } else {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "1 - Įrašyti studentus ir pažymius ranka" << endl;
+        cout << "2 - Generuoti random pažymius" << endl;
+        cout << "3 - Generuoti random studentus ir pažymius" << endl;
+        cout << "4 - Exit" << endl;
+        cout << "Veiksmas: ";
+
+        if (!(cin >> menu_choice)) {        
+            cin.clear();                 
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+            cout << "Įveskite sveikąjį skaičių tarp 1 ir 4." << endl;
+            continue;                     
         }
+
+        if (menu_choice < 1 || menu_choice > 4) {  
+            cout << "Įveskite sveikąjį skaičių tarp 1 ir 4." << endl;
+            continue; 
+        }
+
+        if (menu_choice == 1) {
+            string comm = "y";
+            while (comm == "y") {
+                Student s = enterStudent();
+                s.mean = calculateMean(s.homework, s.exam);
+                s.median = calculateMedian(s.homework, s.exam);
+                students.push_back(s);
+                while (true) {
+                    cout << "Įrašyti dar vieną studentą? (y/n): ";
+                    cin >> comm;
+                    if (comm == "y" || comm == "n") {
+                        break;
+                    }
+                }
+            }
+        } 
+
+
+        int isvedimas;
+        while (true) {
+            cout << "Išvesti rezultatus vidurkio ar medianos pavidalu? (1 - vid., 2 - med.): ";
+            if (cin>>isvedimas) {
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                if (isvedimas == 1 || isvedimas == 2 ) {
+                    break;
+                }
+            } else {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
         
+        }
+
+        cout << left << setw(15) << "Pavardė" << setw(15) << "Vardas" << setw(10) << "Galutinis" << endl;
+        cout << "-------------------------------------------------------------" << endl;
+        for (const auto &stu : students) {
+                printStudent(stu, isvedimas);
+        }
+
+        students.clear();
+
     }
 
-    cout << left << setw(15) << "Pavardė" << setw(15) << "Vardas" << setw(10) << "Galutinis" << endl;
-    cout << "-------------------------------------------------------------" << endl;
-    for (const auto &stu : students) {
-            printStudent(stu, comm2);
-    }
+
     return 0;
 }

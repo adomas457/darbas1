28589201739eb9e2e3e8c9112c38f4a95973a623
Adomas Šapoka <adomas.sapoka@gmail.com>
2026-03-05 00:39:59 +0200

    studento header

---
+++ student.h
@@ -0,0 +1,20 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+#include <vector>
+
+struct Student {
+    std::string name;
+    std::string surname;
+    std::vector<int> homework;
+    int exam;
+    double mean;
+    double median;
+};
+
+double calculateMean(const std::vector<int> &hm, int exam);
+double calculateMedian(std::vector<int> hm, int);
+
+
+#endif

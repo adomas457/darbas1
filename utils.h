24937d8e37ac1ff941453cdbfe135a7ac4fe9f58
Adomas Šapoka <adomas.sapoka@gmail.com>
2026-03-05 09:39:47 +0200

    kitos funk

---
+++ utils.h
@@ -0,0 +1,27 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "student.h"
+#include <string>
+#include <vector>
+#include <limits>
+
+int getInt(const std::string &prompt, int min = std::numeric_limits<int>::min(), int max = std::numeric_limits<int>::max());
+int utf8_length(std::string s);
+
+Student enterStudent();
+Student enterStudent2();
+Student enterStudent3(int count);
+
+std::vector<Student> readFile(const std::string& filename);
+void printStudent(const Student& stu);
+
+bool rusiuoti(const Student& s1, const Student& s2);
+
+extern std::vector<std::string> vardai;
+extern std::vector<std::string> pavardes_vyr;
+extern std::vector<std::string> pavardes_mot;
+extern int rusiavimas;
+
+
+#endif
